24d1730d9369d87f8298f37fa149666862c8ffd2
ayaka <ayaka.murakami1204@gmail.com>
2023-12-10 19:57:11 +0900

    ensyu1

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,17 @@
 
 public class Person {
-
+	public String name=null;
+	public int age=0;
+	
+	public Person(String name,int age) {
+		this.name=name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
 }
